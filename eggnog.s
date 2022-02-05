fa956286a4f8f971509749e14ed3e1a13aff5b4b
Luke Rowe <RogShotz@users.noreply.github.com>
2022-02-04 16:28:51 -0800

    Update eggnog.s

--- eggnog.s
+++ eggnog.s
@@ -1,22 +1,22 @@
-    .global	main
+    .global main
 main:
     // pre-increment the stack point to create space for two 8-byte registers
     // link register (x30), and frame pointer (x29), and store them.
-	stp	x29, x30, [sp, -16]!
+        stp x29, x30, [sp, -16]!
 
     // Load the argument and perform the call. Like 'printf("...")' in C.
-	ldr	x0, =message
-	bl	printf
+        ldr x0, =message
+        bl printf
 
     // initialize the return value in the return register
-	mov	x0, 0
+        mov x0, 0
 
-    // restore the registers and post-decrement 
+    // restore the registers and post-decrement
     // the stack pointer for consistency
-	ldp	x29, x30, [sp], 16
+        ldp x29, x30, [sp], 16
 
     // return from the call
-	ret
+        ret
 
 message:
-	.asciz "Hello, world!\n"
+        .asciz "There once was an egg\nwho noticed it wanted more,\nit became eggnog\n"
